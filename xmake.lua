94611b3276730d6bb26fd873f507db764d1b13ed
baptiste guerin <baptiste.guerin01@etu.umontpellier.fr>
2026-02-06 16:38:57 +0100

    correction xmake

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,12 @@
---packages
+-- Dépendances globales
 add_requires("libsdl2",        {configs={runtimes="MT", shared=false, pic=true}})
 add_requires("libsdl2_image",  {configs={runtimes="MT", shared=false, png=true, jpg=false, tiff=false, webp=false, pic=true}})
 add_requires("libsdl2_mixer",  {configs={runtimes="MT", shared=false, wav=true, mp3=false, flac=false, vorbis=false, pic=true}})
 add_requires("zlib",           {configs={runtimes="MT", shared=false, pic=true}})
-add_requires("quill", {version="11.0.2", configs={runtimes="MT", shared=false, pic=true}})
-add_requires("luajit",         {configs={runtimes="MT", kind="static", pic=true, gc64=true}})
+add_requires("quill",          {version="master", configs={runtimes="MT", shared=false, pic=true}})
+add_requires("luajit",         {version="v2.1", configs={runtimes="MT", kind="static", pic=true, gc64=true}})
 
-
+-- Règle globale
 add_rules("mode.release")
 
 -- Option malloc
@@ -21,41 +21,64 @@ option("debug_mode")
     set_description("Active les logs (Quill)")
     set_showmenu(true)
 
--- Dossier de sortie selon le flag
+-- Dossier de sortie selon la config
 local outdir = "bin/release"
-if has_config("debug_mode") then outdir = "bin/debug" end
-if has_config("malloc_mode") then outdir = "bin/debug_malloc" end
+
+if has_config("debug_mode") then 
+    outdir = "bin/debug"
+end
+
+if has_config("malloc_mode") then
+    outdir = "bin/debug_malloc"
+end
 
 --lib
 target("GrnGame")
     set_kind("shared")
     add_rules("utils.symbols.export_all")
+
     set_languages("c17", "cxx17")
     set_targetdir(outdir)
 
+    -- Sources
     add_files("src/**.c")
     add_files("src/**.cpp")
-    add_headerfiles("src/**.h") 
+    add_headerfiles("src/**.h")
+
     --packages
-    add_packages("libsdl2", "libsdl2_image", "libsdl2_mixer", "zlib", "quill", "luajit")
+    add_packages(
+        "libsdl2",
+        "libsdl2_image",
+        "libsdl2_mixer",
+        "zlib",
+        "quill",
+        "luajit"
+    )
 
     --flags
-    if has_config("debug_mode") then add_defines("DEBUG_MODE") end
-    if has_config("malloc_mode") then add_defines("MALLOC_MODE", "DEBUG_MODE") end
+    if has_config("debug_mode") then
+        add_defines("DEBUG_MODE")
+    end
 
+    if has_config("malloc_mode") then
+        add_defines("MALLOC_MODE")
+        add_defines("DEBUG_MODE")
+    end
+
+    -- Plateforme
     if is_plat("windows") then
-        -- mt
-        set_runtimes("MT") 
+        --runtime statique
+        set_runtimes("MT")
         add_ldflags("/OPT:REF", "/OPT:ICF", {force=true})
-        
+
     elseif is_plat("linux") then
-        
+        --phtread pour sdl 
         add_syslinks("pthread", "dl", "m")
-        -- marche pas
+        --c++ en statique
         add_ldflags("-static-libgcc", "-static-libstdc++", {force=true})
 
     elseif is_plat("macosx") then
-        -- frameworks pour sdl
+        -- Frameworks macOS pour SDL
         add_frameworks("Cocoa", "IOKit", "CoreVideo", "CoreAudio", "AudioToolbox", "Carbon", "ForceFeedback", "Metal")
         add_syslinks("iconv")
     end
@@ -64,22 +87,26 @@ target("GrnGame")
 target("App")
     set_kind("binary")
     set_targetdir(outdir)
-
-    if has_config("debug_mode") then add_defines("DEBUG_MODE") end
-    
+    if has_config("debug_mode") then
+        add_defines("DEBUG_MODE")
+    end
     add_files("src/moteur_lua/lancer_moteur.c")
-    
-    -- Includes pour compilation
+    -- Includes moteur
     add_includedirs("src/headers_sdl")
     add_includedirs("src/bindings_c")
 
-    -- Lien avec le moteur
+    -- Dépendance moteur
     add_deps("GrnGame")
 
     if is_plat("windows") then
+        --runtime statique
         set_runtimes("MT")
+
     elseif is_plat("linux") then
+        -- pour charger libGrnGame.so
         add_rpathdirs("$ORIGIN")
+
     elseif is_plat("macosx") then
+        -- pour charger libGrnGame.dylib
         add_rpathdirs("@executable_path")
     end
