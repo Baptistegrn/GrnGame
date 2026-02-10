7cb511fe831aec91200d9612cbe933640e1ce155
baptiste guerin <baptiste.guerin01@etu.umontpellier.fr>
2026-02-11 00:02:14 +0100

    correction exemple

--- exemples/hotReloadExemple/src/main.lua
+++ exemples/hotReloadExemple/src/main.lua
@@ -1,7 +1,12 @@
 utils.setLogLvl(2)
 --add every files from ../src to path ( dont forget to add folders to path manually if you are unsing it)
 package.path = package.path .. ";../src/?.lua"
+init = false
 function update()
+    if not init then
+        utils.windowed(300, 300)
+        init = true
+    end
     require("drawRectangle").my_function()
 end
 
