892cf5798909cac601354eba9a755ff240c18093
Zetelias <145657074+Zetelias@users.noreply.github.com>
2026-02-16 13:33:19 +0100

    ajout fonctions envoi

--- src/reseau/envoi.c
+++ src/reseau/envoi.c
@@ -3,3 +3,15 @@
 int reseau_envoyer_paquet(ENetPacket *paquet, ENetPeer *peer) {
     return enet_peer_send(peer, 0, paquet);
 }
+
+void reseau_broadcast_paquet(ENetPacket *paquet, ENetHost *host) {
+    enet_host_broadcast(host, 0, paquet);
+}
+
+void reseau_traiter_paquet(ENetPacket *paquet, GestionnaireReseau *gestionnaireReseau) {
+    if (gestionnaireReseau->estServeur) {
+        reseau_broadcast_paquet(paquet, gestionnaireReseau->hote);
+    } else {
+        reseau_envoyer_paquet(paquet, gestionnaireReseau->serveurPeer);
+    }
+}
--- src/reseau/envoi.h
+++ src/reseau/envoi.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "gestionnaire.h"
 #include <enet6/enet.h>
 
 #ifdef __cplusplus
@@ -6,9 +7,19 @@ extern "C" {
 #endif
 
 /**
- * Envoie un paquet au paire
+ * Envoie un paquet au serveur
  */
 int reseau_envoyer_paquet(ENetPacket *paquet, ENetPeer *peer);
+
+/**
+ * Envoie paquet a tous les clients connectées a l'hote
+ */
+void reseau_broadcast_paquet(ENetPacket *paquet, ENetHost *host);
+
+/**
+ * Si on est un serveur, envoie le paquets aux clients, sinon l'envoie au serveur
+ */
+void reseau_traiter_paquet(ENetPacket *paquet, GestionnaireReseau *gestionnaireReseau);
 
 #ifdef __cplusplus
 }
