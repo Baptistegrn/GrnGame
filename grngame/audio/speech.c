f264b2b4d7db2b96346b221ce0a786f83cf1bac5
Zetelias <desertfennek@proton.me>
2026-03-09 12:54:13 +0100

    make speech positional

--- grngame/audio/speech.c
+++ grngame/audio/speech.c
@@ -2,8 +2,12 @@
 
 #include "grngame/core/app.h"
 
+#include <math.h>
+
 void SpeechSay(const char *text, const SoundInfo *sound_info)
 {
+    // TODO have it work with filters like sound
+    
     Speech *speech = g_app.sound_manager.speech;
     Soloud soloud = g_app.sound_manager.soloud;
 
@@ -11,5 +15,12 @@ void SpeechSay(const char *text, const SoundInfo *sound_info)
     Speech_setVolume(speech, sound_info->volume);
     Speech_setLooping(speech, sound_info->looping);
 
-    Soloud_play(soloud, speech);
+    if (!isnan(sound_info->position.x) && !isnan(sound_info->position.y))
+    {
+        Soloud_play3d(soloud, speech, sound_info->position.x, sound_info->position.y, 0.0f);
+    }
+    else
+    {
+        Soloud_play(soloud, speech);
+    }
 }
