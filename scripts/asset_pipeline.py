5f55b343904846f1039fb9ba0ffc926a70b8aa10
Zetelias <desertfennek@proton.me>
2026-03-12 01:03:17 +0100

    start working on std

--- scripts/asset_pipeline.py
+++ scripts/asset_pipeline.py
@@ -6,5 +6,10 @@ def bundle_assets(dir_from: str, dir_to: str) -> None:
     shutil.copytree(dir_from, dir_to, dirs_exist_ok=True, 
                     copy_function=shutil.copy2)
 
+def bundle_std(dir_to: str) -> None:
+    shutil.copytree('std', os.path.join(dir_to, 'scripts', 'std'), dirs_exist_ok=True, 
+                    copy_function=shutil.copy2)
+
 if __name__ == '__main__':
     bundle_assets(sys.argv[1], sys.argv[2])
+    bundle_std(sys.argv[2])
