c9e380568417413a1ab2fdac18b9d4a6cf7f026b
baptiste guerin <baptiste.guerin01@etu.umontpellier.fr>
2026-03-20 23:27:07 +0100

    add pressed and just_pressed and key alias

--- app/main.c
+++ app/main.c
@@ -1,3 +1,24 @@
+
+#include "grngame/core/app.h"
+#include "grngame/dev/logging.h"
+
 int main()
 {
+    AppInfo app;
+    app.name = "Noob - The Factionless";
+    app.version = "1.0.1";
+    app.fps = 80.0;
+    app.window_width = 200;
+    app.window_height = 200;
+    app.window_universe_height = 200;
+    app.window_universe_width = 200;
+    app.asset_folder = "assets";
+    app.window_resizable = true;
+    app.window_fullscreen = false;
+    app.window_maximised = false;
+    app.enable_logs = true;
+    app.log_destination = LOG_TO_CONSOLE;
+    app.force_universe_scale = true;
+    EngineStart(&app);
+    return 0;
 }
