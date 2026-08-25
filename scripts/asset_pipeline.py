6268a10f68084bcb6dbf6bffbf2aa566cc0ec5ad
BaptisteGrn <baptiste.guerin34@gmail.com>
2026-08-25 15:52:50 +0200

    adding test system

--- scripts/asset_pipeline.py
+++ scripts/asset_pipeline.py
@@ -1,16 +1,58 @@
 import sys
-import os
 import shutil
+import os
+from pathlib import Path
 
+
+# Example: dir_from = "tests/pad_event"
 def bundle_assets(dir_from: str, dir_to: str) -> None:
-    shutil.copytree(dir_from, dir_to, dirs_exist_ok=True, 
-                    copy_function=shutil.copy2)
+    shutil.copytree(
+        dir_from,
+        dir_to,
+        dirs_exist_ok=True,
+        copy_function=shutil.copy2,
+    )
+
 
 def bundle_std(dir_to: str) -> None:
-    shutil.copytree('std', os.path.join(dir_to, 'scripts', 'std'), dirs_exist_ok=True, 
-                    copy_function=shutil.copy2,
-                    ignore=shutil.ignore_patterns('main.c', '*.c', '*.h'))
+    dest_path = os.path.join(dir_to, "std")
+    
+    shutil.copytree(
+        "std",
+        dest_path,
+        dirs_exist_ok=True,
+        copy_function=shutil.copy2,
+    )
 
-if __name__ == '__main__':
-    bundle_assets(sys.argv[1], sys.argv[2])
-    bundle_std(sys.argv[2])
+def bundle_controller_db(dir_to: str) -> None:
+    src = Path("grngame/input")
+    dst = Path(dir_to) / "data"
+
+    shutil.copytree(
+        src,
+        dst,
+        dirs_exist_ok=True,
+        copy_function=shutil.copy2,
+        ignore=shutil.ignore_patterns("*.c", "*.cpp", "*.h", "*.hpp"),
+    )
+
+
+def bundle_runtime(dir_to: str) -> None:
+    src = Path("build/Runtime")
+    dst = Path(dir_to)
+
+    shutil.copytree(
+        src,
+        dst,
+        dirs_exist_ok=True,
+        copy_function=shutil.copy2,
+        ignore=shutil.ignore_patterns("*.exp", "*.lib"),
+    )
+
+if __name__ == "__main__":
+    dir_from = sys.argv[1]
+    dir_to = sys.argv[2]
+    bundle_assets(dir_from, dir_to)
+    bundle_std(dir_to)
+    bundle_controller_db(dir_to)
+    bundle_runtime(dir_to)
